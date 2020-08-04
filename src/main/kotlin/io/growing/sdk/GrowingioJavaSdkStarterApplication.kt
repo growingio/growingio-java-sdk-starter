feee2ca8c80237446486068d00bdda91e4f44f7a
IceMimosa <chk19940609@gmail.com>
2020-08-04 10:19:38 +0800

    Init springboot project

---
+++ src/main/kotlin/io/growing/sdk/GrowingioJavaSdkStarterApplication.kt
@@ -0,0 +1,11 @@
+package io.growing.sdk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrowingioJavaSdkStarterApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrowingioJavaSdkStarterApplication>(*args)
+}
---
+++ src/test/kotlin/io/growing/sdk/GrowingioJavaSdkStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.growing.sdk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrowingioJavaSdkStarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
